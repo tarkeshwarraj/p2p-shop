2babe6057b4a05d7b1875b79edbd52618490881f
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-06-08 14:26:59 +0530

    frontend link update

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 //Middleware
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://p2p-shop.vercel.app",
     credentials: true
 }));
 app.use(express.json());
