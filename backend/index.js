c4119f1e81dc1585b5f7b2d235b9995b44e5c4f6
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-06-15 19:34:34 +0530

    frontend url

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 //Middleware
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 app.use(express.json());
