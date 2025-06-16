082a1a58068b84aee336f752e416a9bd510631ce
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-06-16 11:41:39 +0530

    same site

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -47,7 +47,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, //7days
     });
 
